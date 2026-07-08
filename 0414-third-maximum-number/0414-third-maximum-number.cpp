e776a99d6dd89c242cb99c626f126fef1e1315ee
Sayan Sanai <sayansanaicse@gmail.com>
2026-07-08 05:40:48 -0700

    Time: 0 ms (100%), Space: 12.8 MB (93.54%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long first = LONG_MIN;
+        long second = LONG_MIN;
+        long third = LONG_MIN;
+
+        for (int x : nums) {
+            // Ignore duplicates
+            if (x == first || x == second || x == third)
+                continue;
+
+            if (x > first) {
+                third = second;
+                second = first;
+                first = x;
+            }
+            else if (x > second) {
+                third = second;
+                second = x;
+            }
+            else if (x > third) {
+                third = x;
+            }
+        }
+
+        return (third == LONG_MIN) ? first : third;
+    }
+};
